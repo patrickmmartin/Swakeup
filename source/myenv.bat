953e60e4ae02fd5f6264e75146ce20641aa559f7
Patrick Martin <patrickmmartin@gmail.com>
2013-10-27 14:26:35 +0000

    cleanup on the makefiles and targets

--- source/myenv.bat
+++ source/myenv.bat
@@ -1,3 +1,3 @@
 @echo off
-IF "%ProgramFiles(x86)%"=="" SET PATH="C:\PROGRA~1\Borland\Delphi7\bin";"C:\Program Files\Git\cmd";"c:\Program Files\doxygen\bin";C:\Windows\System32 & exit /b
-SET PATH=C:\Delphi7\bin;"C:\Program Files (x86)\Git\cmd";"c:\Program Files\doxygen\bin";C:\Windows\System32
+IF "%ProgramFiles(x86)%"=="" SET PATH="C:\PROGRA~1\Borland\Delphi7\bin";"C:\Program Files\Git\cmd";"c:\Program Files\doxygen\bin";C:\Windows\System32;C:\MinGW\bin & exit /b
+SET PATH=C:\Delphi7\bin;"C:\Program Files (x86)\Git\cmd";"c:\Program Files\doxygen\bin";C:\Windows\System32;C:\MinGW\bin
