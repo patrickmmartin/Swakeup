d3ce41fdef56a481d0c6d4169513b5106711d4d0
Patrick Martin <patrickmmartin@gmail.com>
2013-10-27 22:11:53 +0000

    some cleanup and re-organisation

--- source/swakeup.c
+++ source/swakeup.c
@@ -1,5 +1,5 @@
 /*
- * wakeup.c
+ * swakeup.c
  *
  *  Created on: 26 Oct 2013
  *      Author: Patrick
@@ -7,38 +7,51 @@
 
 #include "stdio.h"
 
+// includes
 #ifdef WIN32
-#include "winsock.h"
+  #include "winsock.h"
 #else
-#include "unistd.h"
-#include "string.h"
-#include "errno.h"
-#include "sys/socket.h"
-#include "netinet/in.h"
-
-// TODO need the platform definitions for these
-#define SOCKET_ERROR -1
-typedef int SOCKET;
-#define closesocket(s) close(s)
+  #include "unistd.h"
+  #include "string.h"
+  #include "errno.h"
+  #include "sys/socket.h"
+  #include "netinet/in.h"
 #endif // WIN32
+
+// platform-specific definitions
+#ifdef WIN32
+#else
+  // standard error for local errors
+  #define SOCKET_ERROR -1
+  // the standard definition is int for a socket
+  typedef int SOCKET;
+  #define closesocket(s) close(s)
+#endif
 
 /**
  * @brief any platform startup actions here
+ * @return non-zero for any startup errors
  */
-int startup();
+int sw_startup();
 
 /**
  * @brief any platform cleanup actions here
+ * @return non-zero for any shutdown errors
  */
-int cleanup();
+int sw_cleanup();
 
 /**
  * pretty prints the socket related error code if non-zero
  * @param code - the code
  * @param op the operation being attempted
  */
-void print_sock_result(int code, const char * op);
+void sw_print_sock_result(int code, const char * op);
 
+/**
+ * wraps up returning a unified socket return code
+ * @return the platform socket error 
+ */
+int sw_error();
 
 const int PHYSADDR_LEN = 6;
 const int MAGICPACKET_LEN = 102;
@@ -46,7 +59,7 @@ const int MAGICPACKET_LEN = 102;
 
 int main(int argc, char * argv[]) {
 
-	print_sock_result(startup(), "startup");
+	sw_print_sock_result(sw_startup(), "startup");
 
 	char MACAddr[] = { 0x00, 0x1D, 0x73, 0x4C, 0x99, 0x2E };
 
@@ -66,15 +79,9 @@ int main(int argc, char * argv[]) {
 	int destination_ip = 0xffffffff; /* broadcast to all */
 	int port = 9;
 
-	SOCKET Sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_IP);
-	if (!Sock) {
-		int err;
-#ifdef WIN32
-		err = WSAGetLastError();
-#else
-		err = errno;
-#endif
-		print_sock_result(err, "socket");
+	SOCKET sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_IP);
+	if (!sock) {
+                sw_print_sock_result(sw_error(), "socket");
 	}
 
 	addr.sin_family = AF_INET;
@@ -85,7 +92,7 @@ int main(int argc, char * argv[]) {
 	// TODO - also root was not required to set this option?
 	if (addr.sin_addr.s_addr == INADDR_BROADCAST) {
 		sooptval = 1;
-		print_sock_result(setsockopt(Sock, SOL_SOCKET, SO_BROADCAST,
+		sw_print_sock_result(setsockopt(sock, SOL_SOCKET, SO_BROADCAST,
 						  (void *) &sooptval, sizeof(sooptval)), "setsockopt");
 	}
 
@@ -95,17 +102,18 @@ int main(int argc, char * argv[]) {
 		memcpy(magicdata + position, MACAddr, PHYSADDR_LEN);
 		position += PHYSADDR_LEN;
 	}
-	/* TODO - i don't like the cast to make the warning go away */
-	retval = sendto(Sock, magicdata, sizeof(magicdata), 0, (struct sockaddr*) &addr, sizeof(addr));
-	if (retval == SOCKET_ERROR)
-		print_sock_result(retval, "sendto");
-	print_sock_result(closesocket(Sock), "closesocket");
 
-	print_sock_result(cleanup(), "cleanup");
+	/* TODO - i don't like the cast to make the warning go away */
+	retval = sendto(sock, magicdata, sizeof(magicdata), 0, (struct sockaddr*) &addr, sizeof(addr));
+	if (retval == SOCKET_ERROR)
+		sw_print_sock_result(retval, "sendto");
+	sw_print_sock_result(closesocket(sock), "closesocket");
+
+	sw_print_sock_result(sw_cleanup(), "cleanup");
 	return 0;
 }
 
-void print_sock_result(int code, const char * op) {
+void sw_print_sock_result(int code, const char * op) {
 
 	if (code == 0)
 		return;
@@ -127,7 +135,7 @@ void print_sock_result(int code, const char * op) {
 
 }
 
-int startup() {
+int sw_startup() {
 
 	int retval = 0;
 #ifdef WIN32
@@ -139,7 +147,7 @@ int startup() {
 	return retval;
 }
 
-int cleanup() {
+int sw_cleanup() {
 #ifdef WIN32
 	return WSACleanup();
 #else
@@ -147,3 +155,11 @@ int cleanup() {
 #endif
 }
 
+int sw_error()
+{
+#ifdef WIN32
+  return WSAGetLastError();
+#else
+  return errno;
+#endif
+}
