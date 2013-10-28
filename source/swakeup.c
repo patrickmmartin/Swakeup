0f8b6430b7ca30a30538b0ed5bf633078d884a64
patrickmmartin@gmail.com <patrickmmartin@gmail.com>
2013-10-28 23:51:35 +0000

    re #10 - closes splint defects

--- source/swakeup.c
+++ source/swakeup.c
@@ -10,6 +10,7 @@
 // includes
 #ifdef WIN32
   #include "winsock.h"
+  #include "stdint.h"
 #else
   #include "unistd.h"
   #include "stdlib.h"
@@ -21,6 +22,7 @@
 
 // platform-specific definitions
 #ifdef WIN32
+  typedef int in_addr_t;
 #else
   // standard error for local errors
   #define SOCKET_ERROR -1
@@ -33,29 +35,29 @@
  * @brief any platform startup actions here
  * @return non-zero for any startup errors
  */
-int sw_startup();
+static int sw_startup();
 
 /**
  * @brief any platform cleanup actions here
  * @return non-zero for any shutdown errors
  */
-int sw_cleanup();
+static int sw_cleanup();
 
 /**
  * pretty prints the socket related error code if non-zero
  * @param code - the code
  * @param op the operation being attempted
  */
-void sw_print_sock_result(int code, const char * op);
+static void sw_print_sock_result(int code, const char * op);
 
 /**
  * wraps up returning a unified socket return code
  * @return the platform socket error 
  */
-int sw_error();
+static int sw_error();
 
-const int PHYSADDR_LEN = 6;
-const int MAGICPACKET_LEN = 102;
+static const size_t PHYSADDR_LEN = 6;
+static const int MAGICPACKET_LEN = 102;
 
 #define usagestr  "%s\n" \
 				  "usage\n" \
@@ -63,7 +65,7 @@ const int MAGICPACKET_LEN = 102;
 				  "macaddress in format aa:bb:cc:dd:ee:ff or aa-bb-cc-dd-ee-ff\n" \
 				  "sends a WOL packet to the local broadcast address on port 9"
 
-void usage(char * arg0, char * reason)
+static void usage(char * arg0, char * reason)
 {
 					  
 	printf(usagestr, reason, arg0);
@@ -71,19 +73,24 @@ void usage(char * arg0, char * reason)
 
 int main(int argc, char * argv[]) {
 
-	unsigned char MACAddr[6];
+	unsigned char MACAddr[6] = {(unsigned char) 0, 
+								(unsigned char) 0, 
+								(unsigned char) 0, 
+								(unsigned char) 0, 
+								(unsigned char) 0, 
+								(unsigned char) 0};
 
 	char *token;
 	char *search = ":-";
-	int i;
+	size_t i;
 	
 	struct sockaddr_in addr;
 	int sooptval;
-	int retval;
-	int position;
+	ssize_t retval;
+	size_t position;
 	char magicdata[MAGICPACKET_LEN];
-	int destination_ip = 0xffffffff; /* broadcast to all */
-	int port = 9;
+	in_addr_t destination_ip = 0xffffffff; /* broadcast to all */
+	uint16_t port = 9;
 	SOCKET sock;
 	
 	if (argc != 2)
@@ -141,38 +148,24 @@ int main(int argc, char * argv[]) {
 	}
 
 	/* TODO - i don't like the cast to make the warning go away */
-	retval = sendto(sock, magicdata, sizeof(magicdata), 0, (struct sockaddr*) &addr, sizeof(addr));
+	retval = sendto(0, magicdata, sizeof(magicdata), 0, (struct sockaddr*) &addr, sizeof(addr));
 	if (retval == SOCKET_ERROR)
-		sw_print_sock_result(retval, "sendto");
+		sw_print_sock_result(SOCKET_ERROR, "sendto");
 	sw_print_sock_result(closesocket(sock), "closesocket");
 
 	sw_print_sock_result(sw_cleanup(), "cleanup");
 	return 0;
 }
 
-void sw_print_sock_result(int code, const char * op) {
+static void sw_print_sock_result(int code, const char * op) {
 
 	if (code == 0)
 		return;
-	char * errorstr = "error message not assigned";
-
-#ifdef WIN32
-	FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
-				  NULL, code, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-				  (LPSTR) errorstr, 0,
-				  (va_list*) NULL);
-#else
-	errorstr = strerror(code);
-#endif
-
-	char buffer[1024];
-	snprintf(buffer, sizeof(buffer) -1,
-			"error in %s - %s", op, errorstr);
-	perror(buffer);
-
+		
+	perror(op);
 }
 
-int sw_startup() {
+static int sw_startup() {
 
 	int retval = 0;
 #ifdef WIN32
@@ -184,7 +177,7 @@ int sw_startup() {
 	return retval;
 }
 
-int sw_cleanup() {
+static int sw_cleanup() {
 #ifdef WIN32
 	return WSACleanup();
 #else
@@ -192,7 +185,7 @@ int sw_cleanup() {
 #endif
 }
 
-int sw_error()
+static int sw_error()
 {
 #ifdef WIN32
   return WSAGetLastError();
